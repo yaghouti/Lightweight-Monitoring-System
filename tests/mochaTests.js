a757d16bbd6f7ed36789a28bc6afeaa05d9b8771
Majid Yaghouti <majid.yaghouti@gmail.com>
2021-06-04 22:19:04 +0430

    Improvement: mocha tests for user groups added.

--- tests/mochaTests.js
+++ tests/mochaTests.js
@@ -1,18 +1,20 @@
-// Hint: To generate mochawesome report, execute mocha mochaTest.js --reporter mochawesome
 const assert = require('chai').assert;
 const axios = require('axios');
 const serverUrl = 'http://localhost:3000/api/v1';
 
-describe('Model', function () {
-  it('Should add a model', async() => {
+describe('User Group', function () {
+  it('Should add a user group', async() => {
     try {
       let response = await axios.post(
-        serverUrl + '/models',
+        serverUrl + '/userGroups',
         {
-          
+          "name": "testUserGroupName",
+          "emails": [
+            "majid.yaghouti@gmail.com"
+          ]
         });
       assert.deepEqual(response.data, {
-        
+        "message": "User group 'testUserGroupName' created."
       });
     }
     catch (e) {
@@ -20,13 +22,13 @@ describe('Model', function () {
     }
   });
 
-  it('Should get models', async() => {
+  it('Should get user groups', async() => {
     try {
-      let response = await axios.get(serverUrl + '/models');
-      assert.typeOf(response.data.models, 'array');
-      response.data.models.forEach(model => {
-        assert.hasAllKeys(model, [
-          'key1', 'key2'
+      let response = await axios.get(serverUrl + '/userGroups');
+      assert.typeOf(response.data.groups, 'array');
+      response.data.groups.forEach(group => {
+        assert.hasAllKeys(group, [
+          'name', 'emails'
         ]);
       });
     }
