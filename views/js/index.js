6dfbacfb5c77c82f761d00e44aac66cf19a531bf
Majid Yaghouti <majid.yaghouti@gmail.com>
2021-06-05 01:52:50 +0430

    Feature: html view for tracking data added.

--- views/js/index.js
+++ views/js/index.js
@@ -2,21 +2,53 @@ window.onload = function () {
   var app = new Vue({
     el: "#app",
     data: {
-      models: []
+      trackers: [],
+      selectedTracker: null,
+      trackingData: [],
+      filteredTrackingData: []
     },
     methods: {
-      loadModels: async function () {
+      loadTrackers: async function () {
         try {
-          let data = await request('GET', '/api/v1/models');
-          this.models = data.models;
+          let data = await request('GET', '/api/v1/trackers');
+          this.trackers = data.trackers;
+          this.trackers.unshift({url: 'Show All'});
+          this.selectedTracker = 'Show All';
         }
         catch (error) {
           alert(error.error.message);
         }
+      },
+      loadTrackingData: async function () {
+        try {
+          let data = await request('GET', '/api/v1/trackers/data');
+          data.forEach(item => {
+            item.time = new Date(item.time);
+          });
+          this.trackingData = data.sort((item1, item2) => item2.time - item1.time);
+          this.buildFilteredTrackingData();
+        }
+        catch (error) {
+          alert(error.error.message);
+        }
+      },
+      onTrackerChange(event) {
+        this.buildFilteredTrackingData();
+      },
+      buildFilteredTrackingData(){
+        if (this.selectedTracker === 'Show All') {
+          this.filteredTrackingData = this.trackingData;
+        }
+        else {
+          this.filteredTrackingData = this.trackingData.filter(item => {
+            return item.url === this.selectedTracker;
+          });
+        }
       }
     }
   });
-  app.loadModels();
+  app.loadTrackers();
+  app.loadTrackingData();
 };
 
 async function request(method, url = '', data = {}) {
